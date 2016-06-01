bdbc69eecb6b4fab2f2d884b6e675484c1ad4675
Michael Arnold <github@razorsedge.org>
2016-06-01 13:24:10 -0700

    Add Package['patch'] and Package['rsync'].

--- puppet/production/manifests/default.pp
+++ puppet/production/manifests/default.pp
@@ -10,15 +10,9 @@ node default {
   service { 'gssproxy': ensure => stopped, enable => false, }
   package { 'deltarpm': ensure => present, }
   package { 'epel-release': ensure => present, }
+  package { 'patch': ensure => present, }
+  package { 'rsync': ensure => present, }
   class { 'jdk_oracle': version => '8', }
-#  file { '/etc/profile.d/java.sh':
-#    ensure  => present,
-#    owner   => 'root',
-#    group   => 'root',
-#    mode    => '0644',
-#    content => "export JAVA_HOME=/usr/java/default\n",
-#    require  => Class['jdk_oracle'],
-#  }
   class { 'git': }
   vcsrepo { '/home/vagrant/spark':
     ensure   => present,
